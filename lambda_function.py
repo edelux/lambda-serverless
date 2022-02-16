57c7a7797500fdf57d8010e9279ae5238a66acfb
Ernesto Deluquez Heilbron <99692694+edelux@users.noreply.github.com>
2022-02-16 00:02:15 -0500

    Update ED

--- lambda_function.py
+++ lambda_function.py
@@ -5,18 +5,19 @@ from boto3.dynamodb.conditions import Key
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
 # Hola a todos estoy con RedHat y los companeritos
+# Modificacion ED, Wed Feb 16 00:00:55 -05 2022
 
 class DynamoAccessor:
     def __init__(self, dynamo_table):
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, cc):
-        response = self.table.query(KeyConditionExpression=Key('cc').eq(cc))
+    def get_data_from_dynamo(self, CC):
+        response = self.table.query(KeyConditionExpression=Key('CC').eq(CC))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
+    db_element = dynamo_backend.get_data_from_dynamo(event['CC'])
     return db_element
 
